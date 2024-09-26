63efd4b31e40b06ffc9ac34ab11a63cdf8a33c7d
MihailMaer <158746471+MihailMaer@users.noreply.github.com>
2024-09-26 09:22:23 +0300

    Add files via upload

---
+++ Cl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Прак2
+{
+    internal class Cl
+    {
+        
+       
+
+        public void summa(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one + num_two);
+        }
+        public void minuc(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one - num_two);
+        }
+        public void multiplications(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one * num_two);
+        }
+        public void division(double num_one, double num_two)
+        {
+            if (num_two == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+            }
+            else
+            {
+                Console.WriteLine(num_one / num_two);
+            }
+
+        }
+        public void sinmultiplications(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Sin(num_one) * num_two);
+        }
+        public void cosmultiplications(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Cos(num_one) * num_two);
+
+        }
+        public void sindivision(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Sin(num_one) / num_two);
+
+        }
+        public void cosdivision(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Cos(num_one) / num_two);
+        }
+        public void arsinmultiolications(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Asin(num_one) * num_two);
+        }
+        public void arcosmultiplications(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Log(num_one) * num_two);
+        }
+        public void PlTR(double num_one, double num_two)
+        {
+            Console.WriteLine(0.5 * (num_one * num_two));
+
+        }
+        public void ScIRCLeD(double num_one, double num_two)
+        {
+            Console.WriteLine((num_one * num_one) / 4 * 3.14);
+        }
+        public void ScIRCLeR(double num_one, double num_two)
+        {
+            Console.WriteLine(3.14 * (num_one * num_one));
+        }
+        public void SKV(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one * num_one);
+        }
+    }
+}
+
+
+
+
+  
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Прак2;
+
+
+start:
+
+Cl clas = new Cl();
+
+string simb;
+double x;
+double y;
+
+Console.WriteLine("Ввести первое число");
+try
+{
+    x = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception)
+{
+
+    x = 0;
+}
+
+
+Console.WriteLine("ввести действие");
+simb = Convert.ToString(Console.ReadLine());
+
+
+Console.WriteLine("ввести второе число");
+try
+{
+    y = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception)
+{
+
+    y = 0;
+}
+
+
+switch (simb)
+        {
+         case "+":
+                clas.summa(x, y);
+                goto start;
+            case "-":
+                clas.minuc(x, y);
+                goto start;
+            case "*":
+                clas.multiplications(x, y);
+                goto start;
+            case "/":
+                clas.division(x, y);
+                goto start;
+            case "sin*":
+                clas.sinmultiplications(x, y);
+                goto start;
+            case "cos*":
+                clas.cosmultiplications(x, y);
+                goto start;
+            case "sin/":
+                clas.sindivision(x, y);
+                goto start;
+            case "cos/":
+                clas.cosdivision(x, y);
+                goto start;
+            case "tan*":
+                clas.arsinmultiolications(x, y);
+                goto start;
+            case "log*":
+                clas.arcosmultiplications(x, y);
+                goto start;
+            case "S-TR":
+                clas.PlTR(x, y);
+                goto start;
+            case "S-C-D":
+                clas.ScIRCLeD(x, y);
+                goto start;
+            case "S-C-R":
+                clas.ScIRCLeR(x, y);
+                goto start;
+            case "S-K":
+                clas.SKV(x, y);
+                goto start;
+        }
